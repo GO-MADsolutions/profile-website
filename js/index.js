af09f49f3c10873e490e5d4bd49fd98aa3802efe
goutham-palani <goutham.palani@freshworks.com>
2021-03-18 00:16:15 +0530

    Add styles to selected link

--- js/index.js
+++ js/index.js
@@ -4,6 +4,16 @@
 
 
 function displaySection(section) {
+  const link = document.querySelectorAll(`[data-link-selected]`);
+  for(let i = 0 ; i < link.length ; i++) {
+    if(link[i].dataset.linkSelected === section) {
+      const element = document.querySelector(`[data-link-selected=${link[i].dataset.linkSelected}]`)
+      element.style.color = "#03CC83";
+    } else {
+      const element = document.querySelector(`[data-link-selected=${link[i].dataset.linkSelected}]`)
+      element.style.color = "#FFFFFF";
+    }
+  }
   const pages = document.getElementById("pages");
   for(let i=0; i< pages.children.length; i++) {
     if(pages.children[i].id === section) {
