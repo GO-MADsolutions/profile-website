36c025f508d08cbc8caf496e0c5e6652f27a7bc4
goutham-palani <goutham.palani@freshworks.com>
2021-03-17 23:58:29 +0530

    Add transition and basic css changes.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-(function () {})();
+(function () {
+  displaySection("home");
+})();
+
+
+function displaySection(section) {
+  const pages = document.getElementById("pages");
+  for(let i=0; i< pages.children.length; i++) {
+    if(pages.children[i].id === section) {
+      const pageElement =  document.getElementById(pages.children[i].id);
+      pageElement.style.display = "block";
+      setTimeout(()=> {
+        pageElement.style.right = "0";
+      },10)
+    }
+    else {
+      const pageElement =  document.getElementById(pages.children[i].id);
+      pageElement.style.display = "none";
+      pageElement.style.right = "-1500px";
+    }
+  }
+}
