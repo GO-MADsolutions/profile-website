8cb5b285c429ef2e5487cc662686b32852299b59
Yogesh J <53269086+Yogesh2806@users.noreply.github.com>
2021-03-20 15:05:46 +0530

    done with all pages

--- js/index.js
+++ js/index.js
@@ -177,13 +177,55 @@ function addStarColor(rating, element) {
   }
 }
 
+// carousel
 /*
-window.onscroll = function () {
-  if (window.innerHeight > ele.getBoundingClientRect().top && !isHighlighted) {
-    isHighlighted = true;
-    const ratingSection = document.querySelectorAll(".rating");
-    for (let i = 0; i < skills.length; i++) {
-      addStarColor(skills[i].stars, ratingSection[i]);
+let span = document.getElementsByTagName("span");
+let div = document.getElementsByTagName("div");
+
+let l = 0;
+span[1].onclick = () => {
+  l++;
+  for (let i of div) {
+    if (l == 0) {
+      i.style.left = "200px";
+    }
+    if (l == 1) {
+      i.style.left = "-1000px";
+    }
+    if (l == 2) {
+      i.style.left = "-2000px";
+    }
+    if (l == 3) {
+      i.style.left = "-3000px";
+    }
+    if (l == 4) {
+      i.style.left = "=4000px";
+    }
+    if (l > 4) {
+      l = 4;
+    }
+  }
+};
+span[0].onclick = () => {
+  l--;
+  for (let i of div) {
+    if (l == 0) {
+      i.style.left = "200px";
+    }
+    if (l == 1) {
+      i.style.left = "-1000px";
+    }
+    if (l == 2) {
+      i.style.left = "-2000px";
+    }
+    if (l == 3) {
+      i.style.left = "-3000px";
+    }
+    if (l == 4) {
+      i.style.left = "-4000px";
+    }
+    if (l < 4) {
+      l = 0;
     }
   }
 };
